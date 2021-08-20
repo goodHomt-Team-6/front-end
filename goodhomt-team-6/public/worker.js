5fced74477d7bd4d6e0bc7057974fee126eb1741
Woo Jieum <oozeume@gmail.com>
2021-08-20 14:49:59 +0900

    CORRECT : serviceWorker 관련 파일 주석처리 해제

--- goodhomt-team-6/public/worker.js
+++ goodhomt-team-6/public/worker.js
@@ -1,55 +1,55 @@
-// var CACHE_NAME = 'pwa-task-manager';
-// var urlsToCache = ['/', '/completed'];
-// // Install a service worker
-// self.addEventListener('install', (event) => {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(function (cache) {
-//       console.log('Opened cache');
-//       return cache.addAll(urlsToCache);
-//     }),
-//   );
-// });
-// // Cache and return requests
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       // Cache hit - return response
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     }),
-//   );
-// });
-// // Update a service worker
-// self.addEventListener('activate', (event) => {
-//   var cacheWhitelist = ['pwa-task-manager'];
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (cacheWhitelist.indexOf(cacheName) === -1) {
-//             return caches.delete(cacheName);
-//           }
-//         }),
-//       );
-//     }),
-//   );
-// });
+var CACHE_NAME = 'pwa-task-manager';
+var urlsToCache = ['/', '/completed'];
+// Install a service worker
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
+// Update a service worker
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
 
-// self.addEventListener('fetch', (e) => {
-//   e.respondWith((async () => {
-//     // 1. 캐시에서 먼저 찾아 봄
-//     const r = await caches.match(e.request);
-//     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-//     if (r) { return r; }
-//     // 2. 없으면 직접 fetch해서 데이터 서버로부터 가져옴
-//     const response = await fetch(e.request);
-//     const cache = await caches.open(cacheName);
-//     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-//     // 3. 캐시에 저장
-//     cache.put(e.request, response.clone());
-//     return response;
-//   })());
-// });
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    // 1. 캐시에서 먼저 찾아 봄
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    // 2. 없으면 직접 fetch해서 데이터 서버로부터 가져옴
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    // 3. 캐시에 저장
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
